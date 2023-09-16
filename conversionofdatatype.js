cdf2ebfb5d223c93249c4c14e06e1ddaa2ad87ed
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-16 12:15:27 +0530

    conversion
    conversion of datatypes

---
+++ conversionofdatatype.js
@@ -0,0 +1,17 @@
+//we can convert any datatype to other datatype it can be converted but output can differ if ....
+let age = true
+let agestring = Number(age)
+console.log(typeof agestring)
+console.log(agestring)
+/* "123" -> 123
+null -> 0
+"123abc" -> Nan 
+undefined -> nan
+
+*/
+// boolean true - 1
+// false - 0
+// " " - false
+// "devesh" - true
+// we can convert any datatype to other by Datatype(variablename)
+
