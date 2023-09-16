d00c8daa534ed795d8ba9840bf3f18029fd75fbc
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-16 11:27:52 +0530

    variables
    details about variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+const name = "devesh"
+let id = "1234"
+var idname = "college id"
+collegename = "gl bajaj"
+//we cannot change constant value once declared
+//we can change var and let anytime
+// in line 4 the way we declared variable we can but it is not good way
+// name = "durgesh" we cannot do this
+// console.log(name)
+// we simply use var and let for declaring variable
+//var is not used mostly because of block scope
+id = "45677"
+idname = "newcollegeid"
+collegename = "none"
+let idd //it gives undefined statement 
+console.table([id,idname,collegename,idd])
