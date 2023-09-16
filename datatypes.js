c05f887458b26fc5dad052ccfdbbf79f29fbea59
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-16 11:53:05 +0530

    datatypes
    datatypes

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+//code readability matters a lot in js it helps us understanding code and its usage
+//mdn and ecma are two documentation used to understand js deeply 
+let name = "devesh" // string
+let age = 10 // number
+let married = false // boolean
+let newname = null // object
+//these all are data types and like number have some range 2 to the power 53
+//we use bigint for more range
+//symbol is datatye used for unique values
+// null matlab ye nahi ki wo undefined hai iska matlab ye hai ki wo khali hai
+console.log(typeof name)
+//it tells which type of data type is used
