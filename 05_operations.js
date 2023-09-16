4b5f51da0d8fb3d9f47bea031abc115d223afef9
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-16 12:34:36 +0530

    Create 05_operations.js
    operations in js

---
+++ 05_operations.js
@@ -0,0 +1,12 @@
+// we know we can add sub mul div modulo and power the number
+let str1 = "devesh "
+let str2 = "upadhyay"
+let str3 = str1 + str2
+console.log(str1+str2)
+console.log(str3)
+console.log(1+2+"3")
+console.log("1"+2+3) // -> 123 agar string first hai to savi ko string kiya jaaega 
+console.log(+true) // -> 1
+let num = 4
+console.log(++num) // first assign the print 
+//num++ means first print then assign
