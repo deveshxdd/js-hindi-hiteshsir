21379dce60e5a22a1cfe3ce5b1e3ae0e038fff9b
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-24 10:34:06 +0530

    Create 06_Stackandheapmemory.js

---
+++ 06_Stackandheapmemory.js
@@ -0,0 +1,17 @@
+// stack (primitive)
+// heap (non primitive)
+let myname = "devesh"
+let newname = myname // ye upar wale ki copy value lega 
+newname = "aakash" // isse myname change nhi hoga
+console.log(myname)
+console.log(newname)
+let userone ={
+email: "iamdevesh@gmail.com",
+upi : "devesh@ybl"
+}
+let usertwo = userone // ye heap me uski reference lelega aur isme change krenge to original me change hoga 
+//kyuki ye copy value nhi leta hai
+usertwo.email = "hjfdjf"
+usertwo.upi = "ybbl"
+console.log(userone.email)
+console.log(userone.upi)
