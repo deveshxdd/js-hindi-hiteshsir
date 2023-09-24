f9b7f52e75a8bfb789ebfc1a17ad55bfa63e2550
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-24 13:14:46 +0530

    Create 09_dateandtime.js
    DATE AND TIME

---
+++ 09_dateandtime.js
@@ -0,0 +1,21 @@
+// let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.getDay()+1)
+// console.log(mydate.getMonth()+1)
+//OUTPUS //
+// 2023-09-24T07:34:52.247Z
+// Sun Sep 24 2023 13:04:52 GMT+0530 (India Standard Time)
+// 24/9/2023, 1:04:52 pm
+// 1
+// 8
+let mydate = new Date(2023,3,12 , 12,6,12)
+console.log(mydate.toString())
+console.log(mydate.getDay())
+console.log(mydate.getTime())
+//OUTPUT
+// Wed Apr 12 2023 12:06:12 GMT+0530 (India Standard Time)
+// 3
+// 1681281372000
+//HUMLOG DATE OBJECT KO KHUD SE CUSTOMIZE V KARSKTE H WO NEXT ME PATA CHALEGA
