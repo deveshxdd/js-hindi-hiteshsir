4dd903fca855928b2e89be6e191db60c124b3be6
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-09-27 23:29:54 +0530

    objects

--- 12_objectsadvance.js
+++ 12_objectsadvance.js
@@ -3,7 +3,7 @@
      jsuserr.name = "devesh"
      jsuserr.email = "udevesh929@gmail.com"
      jsuserr.id = "1,2,3,4,5"
-     console.log(jsuserr)
+    //  console.log(jsuserr)
       /// NESTED OBJECTS //
       const obj = {
         email: "rcrvcv" ,
@@ -14,6 +14,26 @@ username:{
 }
         }
       }
-      console.log(obj.fullname.username.firstname)
+    //   console.log(obj.fullname.username.firstname)
         /// how to merge two objects
-        
+        const obj1 = {
+            name: "dev" ,
+            email: "iamrrc"
+        }
+        const obj2 = {
+            username: "dev" ,
+            id: "iamrrc"
+        }
+        // const obj3 = {obj1 , obj2}
+        // console.log(obj3)  this is not good way to combine
+        // const obj3 = Object.assign({}, obj1 , obj2 , obj)
+        // console.log(obj3);  this is also not a good way
+        // best way spread 
+        const obj3 = {...obj1 , ...obj2}
+        console.log(obj3);
+        // most important concept
+        console.log(Object.keys(jsuserr));
+        console.log(Object.values(jsuserr));
+        console.log(Object.entries(jsuserr ));
+        // this meathod converts the object value into array
+        // further we can use loops  
